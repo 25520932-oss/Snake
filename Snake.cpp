ca5df17342464a8de39b7da1dc0e866737f5f05d
Phạm Lê Chí Công <congp904@gmail.com>
2026-03-23 16:11:24 +0700

    Sửa lỗi để game tương thích với cả hệ điều hành MacOs và Windows

--- Snake.cpp
+++ Snake.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
-#include <windows.h>
 #include <cstdlib>
-#include <conio.h>
+#include <cstdio>
+
+    //Hải quan hệ điều hành
+#ifdef _WIN32 
+    // Nếu là Windows thì nạp thư viện này
+    #include <windows.h>
+    #include <conio.h>
+#else 
+    // Nếu là Mac/Linux thì nạp thư viện này
+    #include <unistd.h>
+    #include <termios.h>
+    #include <fcntl.h>
+#endif
+
 using namespace std;
+
 void gotoxy( int column, int line );
+#ifndef _WIN32
+int kbhit(void);
+int getch(void);
+#endif
 struct Point{
     int x,y;
 };
@@ -34,6 +51,22 @@ public:
     }
 };
 
+void gotoxy(int column, int line)
+{
+    #ifdef _WIN32
+    // Code cho Windows
+    COORD coord;
+    coord.X = column;
+    coord.Y = line;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+#else
+    // Code cho Mac
+    printf("\033[%d;%dH", line, column);
+#endif
+}
+#ifndef _WIN32
+
+
 int main()
 {
     CONRAN r;
@@ -46,25 +79,65 @@ int main()
             if (t=='a') Huong = 2;
             if (t=='w') Huong = 3;
             if (t=='d') Huong = 0;
-            if (t=='x') Huong = 1;
+            if (t=='s') Huong = 1; 
         }
-        system("cls");
+        
+        // Trạm kiểm soát lệnh xóa màn hình
+        #ifdef _WIN32
+            system("cls");
+        #else
+            system("clear");
+        #endif
+
         r.Ve();
         r.DiChuyen(Huong);
-        Sleep(300);
+        
+        // Trạm kiểm soát lệnh delay thời gian
+        #ifdef _WIN32
+            Sleep(300);
+        #else
+            usleep(300000);
+        #endif
     }
 
     return 0;
 }
 
 
-void gotoxy( int column, int line )
-  {
-  COORD coord;
-  coord.X = column;
-  coord.Y = line;
-  SetConsoleCursorPosition(
-    GetStdHandle( STD_OUTPUT_HANDLE ),
-    coord
-    );
-  }
+
+
+
+int kbhit(void) {
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+    if(ch != EOF) {
+        ungetc(ch, stdin);
+        return 1;
+    }
+    return 0;
+}
+
+// Hàm lấy ký tự vừa bấm
+int getch(void) {
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr( STDIN_FILENO, &oldattr );
+    newattr = oldattr;
+    newattr.c_lflag &= ~( ICANON | ECHO );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+    return ch;
+}
+#endif
