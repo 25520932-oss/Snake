50b97c0da90c87453f58fba6d4a950970ae23d87
Tan <you@example.com>
2026-03-27 22:06:51 +0700

    Them thuc an va ve khung

--- Snake.cpp
+++ Snake.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstdio>
-
+#include <locale>
+#include <ctime>
     //Hải quan hệ điều hành
-#ifdef _WIN32 
+#ifdef _WIN32
     // Nếu là Windows thì nạp thư viện này
     #include <windows.h>
     #include <conio.h>
-#else 
+#else
     // Nếu là Mac/Linux thì nạp thư viện này
     #include <unistd.h>
     #include <termios.h>
     #include <fcntl.h>
 #endif
+
+#define WIDTH 40
+#define HEIGHT 20
 
 using namespace std;
 
@@ -28,6 +32,7 @@ class CONRAN{
 public:
     struct Point A[100];
     int DoDai;
+    Point food;
     CONRAN(){
         DoDai = 3;
         A[0].x = 10; A[0].y = 10;
@@ -49,6 +54,42 @@ public:
         if (Huong==3) A[0].y = A[0].y - 1;
 
     }
+    void TaoThucAn(){
+    food.x = rand() % (WIDTH-1) + 1;
+    food.y = rand() % (HEIGHT-1) + 1;
+    }
+    void VeThucAn(){
+    gotoxy(food.x, food.y);
+    cout<<"O";
+    }
+    void TangDoDai(){
+        if(A[0].x==food.x&&A[0].y==food.y) {
+            DoDai++;
+            TaoThucAn();
+        }
+    }
+    void VeKhung(){
+        for(int i = 0; i < HEIGHT; i++) {
+        for(int j = 0; j < WIDTH; j++) {
+
+            // Góc
+            if(i == 0 && j == 0) cout << "o";
+            else if(i == 0 && j == WIDTH - 1) cout << "o";
+            else if(i == HEIGHT - 1 && j == 0) cout << "o";
+            else if(i == HEIGHT - 1 && j == WIDTH - 1) cout << "o";
+
+            // Cạnh trên & dưới
+            else if(i == 0 || i == HEIGHT - 1) cout << "-";
+
+            // Cạnh trái & phải
+            else if(j == 0 || j == WIDTH - 1) cout << "|";
+
+            // Bên trong
+            else cout << " ";
+        }
+        cout << endl;
+    }
+    }
 };
 
 void gotoxy(int column, int line)
@@ -64,14 +105,19 @@ void gotoxy(int column, int line)
     printf("\033[%d;%dH", line, column);
 #endif
 }
-#ifndef _WIN32
+
 
 
 int main()
 {
+    srand(time(0));
     CONRAN r;
     int Huong = 0;
     char t;
+    int width = 40;
+    int height = 20;
+
+    r.TaoThucAn();
 
     while (1){
         if (kbhit()){
@@ -79,19 +125,22 @@ int main()
             if (t=='a') Huong = 2;
             if (t=='w') Huong = 3;
             if (t=='d') Huong = 0;
-            if (t=='s') Huong = 1; 
+            if (t=='s') Huong = 1;
         }
-        
+
         // Trạm kiểm soát lệnh xóa màn hình
         #ifdef _WIN32
             system("cls");
         #else
-            system("clear");
+            systemwwww("clear");
         #endif
 
+        r.VeKhung();
+        r.VeThucAn();
         r.Ve();
         r.DiChuyen(Huong);
-        
+        r.TangDoDai();
+
         // Trạm kiểm soát lệnh delay thời gian
         #ifdef _WIN32
             Sleep(300);
@@ -103,41 +152,48 @@ int main()
     return 0;
 }
 
+#ifndef _WIN32
 
-
-
-
-int kbhit(void) {
+int kbhit(void){
     struct termios oldt, newt;
     int ch;
     int oldf;
+
     tcgetattr(STDIN_FILENO, &oldt);
     newt = oldt;
     newt.c_lflag &= ~(ICANON | ECHO);
+
     tcsetattr(STDIN_FILENO, TCSANOW, &newt);
 
     oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
     fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
     ch = getchar();
+
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
     fcntl(STDIN_FILENO, F_SETFL, oldf);
-    if(ch != EOF) {
+
+    if (ch != EOF){
         ungetc(ch, stdin);
         return 1;
     }
+
     return 0;
 }
 
-// Hàm lấy ký tự vừa bấm
-int getch(void) {
+int getch(void){
     struct termios oldattr, newattr;
     int ch;
-    tcgetattr( STDIN_FILENO, &oldattr );
+
+    tcgetattr(STDIN_FILENO, &oldattr);
     newattr = oldattr;
-    newattr.c_lflag &= ~( ICANON | ECHO );
-    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    newattr.c_lflag &= ~(ICANON | ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
     ch = getchar();
-    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
     return ch;
 }
+
 #endif
